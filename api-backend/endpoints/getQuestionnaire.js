5b9065160ba9bbcfa9887c365322c194543f2eea
Themis00 <ef2000th@gmail.com>
2023-01-19 18:18:47 +0200

    get questionnaire info
    In this endpoint, we get an object that includes the questionnaireTitle,
    and the questions of the questionnaire with all their attributes

---
+++ api-backend/endpoints/getQuestionnaire.js
@@ -0,0 +1,35 @@
+/*{baseURL}/questionnaire/:questionnaireID
+
+In this endpoint, we get an object that includes the questionnaireTitle,
+and the questions of the questionnaire with all their attributes
+*/
+
+const express = require('express');
+const router = express.Router();
+var mysql = require('mysql');
+
+function getQuestionnaire(req,res){
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "intelliq",
+        multipleStatements: true
+      });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        let myquery= "select questionnaireTitle from `questionnaire` where questionnaireID = req.params.questionnaireID;\
+                        select keyword from `keywords` where questionnaireID = req.params.questionnaireID;\
+                        select qID, qtext, required, type from `questions` where questionnaireID = req.params.questionnaireID order by qID;";
+        con.query(myquery, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+
+}
+
+router.get('/questionnaire/:questionnaireID',getQuestionnaire)
+module.exports = router; 
