1d0a1161d4e78b77a26c422f3ce4c15c899c73f4
ntua-el14905 <115491843+ntua-el14905@users.noreply.github.com>
2023-01-20 18:46:29 +0200

    Add files via upload

--- api-backend/index.js
+++ api-backend/index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express();
 const port = 9103;
+//const bodyparser = require('body-parser');
 var path = require('path');
 
-
+//middlewares
+//app.use(bodyParser.json());
 
 //initialize port for node application to run
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`inteliQ listening on port ${port}!`);
 });
-
-
-
 
 
 app.get('/', (req, res) => {
@@ -19,27 +18,3 @@ app.get('/', (req, res) => {
 });
 
 
-// load all endpoints
-const healthcheck=require("./admin/healtcheck.js");
-const questionnaire_upd=require("./admin/questionnaire_upd.js");
-const resetall=require("./admin/resetall");
-const resetq=require("./admin/resetq");
-//const dataByName=require("./admin/dataByName");
-
-const questionnaire=require("./questionnaire.js");
-const question=require("./question.js");
-const doanswer=require("./doanswer.js");
-const getsessionanswers=require("./getsessionanswers.js");
-
-
-//bind all endpoints to app router
-app.use('/',healthcheck);
-app.use('/',questionnaire_upd);
-app.use('/',resetall);
-app.use('/',resetq);
-//app.use('/',dataByName);
-
-app.use('/',questionnaire);
-app.use('/',question);
-app.use('/',doanswer);
-app.use('/',getsessionanswers);
