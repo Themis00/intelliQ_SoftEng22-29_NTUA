a93fae82610ffc3ea4e96b57bac00cd6c6445a3b
ntua-el18129 <el18129@mail.ntua.gr>
2023-02-01 01:13:00 +0200

    Async/await usage
    Async/await στο αρχείο getQuestion.js

--- api-backend/endpoints/getQuestion.js
+++ api-backend/endpoints/getQuestion.js
@@ -9,26 +9,32 @@ const router = express.Router();
 var mariadb = require('mariadb/callback');
 var path = require('path');
 
-function getQuestionRequest(req,res){
+async function getQuestionRequest(req,res){
 
     const pool = require(path.resolve("db_connection/getPool.js"));
-    pool.getConnection(function(err,connection) {
+
+    await new Promise(() => pool.getConnection(async function(err,connection) {
+        
         if (err) throw err;
         console.log("Connected to db");
+
         let myquery= "select questionnaireID, qID, qtext, required, type from `questions` where questionnaireID =" + "'" + req.params.questionnaireID+"'"+ "and qID =" + "'" + req.params.questionID+"';"+
             "select optID, opttxt, nextqID from `options` where questionnaireID =" + "'" + req.params.questionnaireID+"'"+ "and qID =" + "'" + req.params.questionID+"'"+ "order by optID";
-        connection.query(myquery, function (err, result, fields) {
+
+        await new Promise(() => connection.query(myquery, function (err, result, fields) {
             if (err) throw err;
             //------------------ Modify JSON in order to have the wanted syntax -----------------------
-            let temp1 = JSON.stringify(result[0][0]).slice(0,-1)
-            let temp2 = JSON.stringify(result[1])
+            let temp1 = JSON.stringify(result[0][0]).slice(0,-1);
+            let temp2 = JSON.stringify(result[1]);
             let json_str = temp1 + ",\"options\":" + temp2 + "}";
             //-----------------------------------------------------------------------------------------
             res.status(200).send(JSON.parse(json_str));
-        });
+        }));
+
         connection.release();
         console.log("Disconnected from db");
-    });
+
+    }));
 
 }
 
